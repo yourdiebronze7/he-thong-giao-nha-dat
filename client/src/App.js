310df3dcc13fc92df891b93eed997cbe64812bf9
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-31 09:08:00 +0000

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chào mừng đến với hệ thống giao dịch bất động sản!</h1>
+    </div>
+  );
+}
+
+export default App;
