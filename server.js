73ec1d4129c339d86af291e33286c8b882115204
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-31 09:08:53 +0000

    Updated greeting message in the response.

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hệ thống giao dịch bất động sản đã sẵn sàng!');
+  res.send('Welcome to the Real Estate Transaction System!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy trên cổng ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
